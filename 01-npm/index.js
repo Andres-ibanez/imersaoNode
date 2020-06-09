3ed8004e38c0dd4c6439cb9b452fec642494cd9a
Andres <cristian.ibane@aluno.unila.edu.br>
2020-06-09 17:53:17 -0300

    feat: Aula 01

---
+++ 01-npm/index.js
@@ -0,0 +1,9 @@
+// Probando a consola e o npm init -y para iniciar repositorio com opções default
+
+console.log('Hello node!')
+
+// Declarando variaveis 
+
+const a = 1
+const b = 2
+console.log(a + b)
